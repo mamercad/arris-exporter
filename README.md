93991b1ea077f367179986a2ca7e4782f9da5c4c
Mark Mercado <mamercad@gmail.com>
2020-07-16 05:39:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# arris-exporter
+
+Prometheus exporter for my Arris SBG10. If they'd only offer SNMP, sigh. This is much hacks, but, I wanted something relatively quick (to be able to monitor correctables and uncorrectables without logging into a web browser manually).
+
+```bash
+❯ virtualenv -p python3 venv
+Running virtualenv with interpreter /usr/local/bin/python3
+Already using interpreter /usr/local/opt/python/bin/python3.7
+Using base prefix '/usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7'
+New python executable in /Users/mark/Code/github.com/mamercad/arris-exporter/venv/bin/python3.7
+Also creating executable in /Users/mark/Code/github.com/mamercad/arris-exporter/venv/bin/python
+Installing setuptools, pip, wheel...
+done.
+
+❯ source venv/bin/activate
+
+❯ pip install -r requirements.txt
+Processing /Users/mark/Library/Caches/pip/wheels/0a/9e/ba/20e5bbc1afef3a491f0b3bb74d508f99403aabe76eda2167ca/bs4-0.0.1-py3-none-any.whl
+Collecting mechanize
+  Using cached mechanize-0.4.5-py2.py3-none-any.whl (109 kB)
+Collecting selenium
+  Using cached selenium-3.141.0-py2.py3-none-any.whl (904 kB)
+Collecting beautifulsoup4
+  Using cached beautifulsoup4-4.9.1-py3-none-any.whl (115 kB)
+Collecting html5lib>=0.999999999
+  Using cached html5lib-1.1-py2.py3-none-any.whl (112 kB)
+Collecting urllib3
+  Using cached urllib3-1.25.9-py2.py3-none-any.whl (126 kB)
+Collecting soupsieve>1.2
+  Using cached soupsieve-2.0.1-py3-none-any.whl (32 kB)
+Collecting six>=1.9
+  Using cached six-1.15.0-py2.py3-none-any.whl (10 kB)
+Collecting webencodings
+  Using cached webencodings-0.5.1-py2.py3-none-any.whl (11 kB)
+Installing collected packages: soupsieve, beautifulsoup4, bs4, six, webencodings, html5lib, mechanize, urllib3, selenium
+Successfully installed beautifulsoup4-4.9.1 bs4-0.0.1 html5lib-1.1 mechanize-0.4.5 selenium-3.141.0 six-1.15.0 soupsieve-2.0.1 urllib3-1.25.9 webencodings-0.5.1
+
+❯ ./arris.py --help
+usage: arris.py [-h] [--loginurl LOGINURL] [--username USERNAME]
+                [--password PASSWORD] [--listen LISTEN]
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --loginurl LOGINURL
+  --username USERNAME
+  --password PASSWORD
+  --listen LISTEN
+
+❯ ./arris.py --password supersecretpassword --listen 8000
+127.0.0.1 - - [16/Jul/2020 05:35:03] "GET /metrics HTTP/1.1" 200 -
+
+❯ curl http://localhost:8000/metrics
+arris_downstreams_dcid{name="Downstream 1"} 17
+arris_downstreams_frequency{name="Downstream 1"} 579.00
+arris_downstreams_power{name="Downstream 1"} -1.60
+arris_downstreams_snr{name="Downstream 1"} 38.61 dB
+arris_downstreams_octets{name="Downstream 1"} 7042634012
+arris_downstreams_correcteds{name="Downstream 1"} 41
+arris_downstreams_uncorrectables{name="Downstream 1"} 0
+...
+```
---
+++ arris.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+
+import argparse, re
+import http.server
+import socketserver
+from bs4 import BeautifulSoup
+from pprint import pprint
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--loginurl', default='http://192.168.100.1/cgi-bin/basic_pwd_cgi')
+parser.add_argument('--username', default='admin')
+parser.add_argument('--password', default='password')
+parser.add_argument('--listen', type=int, default=8000)
+args = parser.parse_args()
+
+def scrape():
+  chrome_options = Options()
+  chrome_options.add_argument("--headless")
+  browser = webdriver.Chrome(options=chrome_options)
+  browser.get(args.loginurl)
+  sleep(2)
+  user = browser.find_elements_by_xpath('//*[@id="user"]')
+  user[0].send_keys(args.username)
+  pwd = browser.find_elements_by_xpath('//*[@id="pwd"]')
+  pwd[0].send_keys(args.password)
+  submit = browser.find_elements_by_xpath('/html/body/div[1]/div[3]/form/p[3]/input[1]')
+  submit[0].click()
+  sleep(2)
+  status = browser.page_source
+
+  # with open('status.html') as f:
+  #   status = f.read()
+
+  soup = BeautifulSoup(status, 'html.parser')
+  tds = soup.find_all('td')
+
+  downstreams = []
+  for i in range(10,10+9*16,9):
+    downstreams.append({
+      'name': tds[i+0].text,
+      'dcid': tds[i+1].text,
+      'freq': tds[i+2].text,
+      'power': tds[i+3].text,
+      'snr': tds[i+4].text,
+      'mod': tds[i+5].text,
+      'octets': tds[i+6].text,
+      'corr': tds[i+7].text,
+      'uncorr': tds[i+8].text,
+    })
+  # pprint(downstreams)
+
+  upstreams = []
+  for i in range(162,162+7*4,7):
+    upstreams.append({
+      'name': tds[i+0].text,
+      'ucid': tds[i+1].text,
+      'freq': tds[i+2].text,
+      'power': tds[i+3].text,
+      'chan': tds[i+4].text,
+      'symb': tds[i+5].text,
+      'mod': tds[i+6].text,
+    })
+  # pprint(upstreams)
+
+  status = {
+    'uptime': tds[192].text,
+    'computers': tds[194].text.rstrip(),
+    'status': tds[196].text,
+    'date': tds[198].text,
+  }
+  # pprint(status)
+
+  interfaces = []
+  for i in range(205,205+5*5,5):
+    interfaces.append({
+      'name': tds[i+0].text,
+      'prov': tds[i+1].text,
+      'state': tds[i+2].text,
+      'speed': tds[i+3].text,
+      'mac': tds[i+4].text,
+    })
+  # pprint(interfaces)
+
+  html = []
+
+  for downstream in downstreams:
+    html.append('arris_downstreams_dcid{{name="{}"}} {}'.format(downstream['name'], downstream['dcid']))
+    freq = downstream['freq'].replace(' MHz', '')
+    html.append('arris_downstreams_frequency{{name="{}"}} {}'.format(downstream['name'], freq))
+    power = downstream['power'].replace(' dBmV', '')
+    html.append('arris_downstreams_power{{name="{}"}} {}'.format(downstream['name'], power))
+    snr = downstream['snr'].replace(' dbB', '')
+    html.append('arris_downstreams_snr{{name="{}"}} {}'.format(downstream['name'], snr))
+    html.append('arris_downstreams_octets{{name="{}"}} {}'.format(downstream['name'], downstream['octets']))
+    html.append('arris_downstreams_correcteds{{name="{}"}} {}'.format(downstream['name'], downstream['corr']))
+    html.append('arris_downstreams_uncorrectables{{name="{}"}} {}'.format(downstream['name'], downstream['uncorr']))
+
+  for upstream in upstreams:
+    html.append('arris_upstreams_ucid{{name="{}"}} {}'.format(upstream['name'], upstream['ucid']))
+    freq = upstream['freq'].replace(' MHz', '')
+    html.append('arris_upstreams_frequency{{name="{}"}} {}'.format(upstream['name'], freq))
+    power = upstream['power'].replace(' dBmV', '')
+    html.append('arris_upstreams_power{{name="{}"}} {}'.format(upstream['name'], power))
+    symb = upstream['symb'].replace(' kSym/s', '')
+    html.append('arris_upstreams_symbolrate{{name="{}"}} {}'.format(upstream['name'], symb))
+
+  for interface in interfaces:
+    if interface['prov'] == 'Enabled':
+      html.append('arris_interfaces_provisioned{{name="{}"}} {}'.format(interface['name'], 1))
+    else:
+      html.append('arris_interfaces_provisioned{{name="{}"}} {}'.format(interface['name'], 0))
+    if interface['state'] == 'Up':
+      html.append('arris_interfaces_state{{name="{}"}} {}'.format(interface['name'], 1))
+    else:
+      html.append('arris_interfaces_state{{name="{}"}} {}'.format(interface['name'], 0))
+
+  # hacky garbage
+  minutes = 0
+  p = re.compile('(\d+)\s+d:\s+(\d+)\s+h:\s+(\d+)\s+m')
+  m = p.match(status['uptime'])
+  if m:
+    x = list(map(int, m.groups()))
+    minutes += x[0] * 1440 # days  * minutes/day
+    minutes += x[1] * 60   # hours * minutes/hour
+    minutes += x[2]        # minutes
+    html.append('arris_uptime_minutes {}'.format(minutes))
+
+  return('\n'.join(html)+'\n')
+
+def serve():
+  class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+      if self.path == '/':
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        html = f"<html><head></head><body><h1>arris-exporter</h1></body></html>"
+        self.wfile.write(bytes(html, "utf8"))
+        return
+      if self.path == '/metrics':
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        html = scrape()
+        self.wfile.write(bytes(html, "utf8"))
+  handler_object = MyHttpRequestHandler
+  PORT = args.listen
+  my_server = socketserver.TCPServer(("", PORT), handler_object)
+  my_server.serve_forever()
+
+if __name__ == '__main__':
+  serve()
